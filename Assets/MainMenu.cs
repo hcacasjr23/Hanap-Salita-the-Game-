3301970dc1a9179158c3462b7770263a13a3c20a
hcacasjr23 <hdcjr00@gmail.com>
2021-06-16 00:39:45 +0800

    Added new files and Edited Main Menu UI

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public void PlayGame()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
+  public void ExitGame()
+  {
+    Debug.Log("QUIT!");;
+    Application.Quit();
+  }
+
 }
